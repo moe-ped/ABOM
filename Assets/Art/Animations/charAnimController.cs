83c5924a6ee37bc80bafb385888fc95530e9e12e
ldaldrup <ldaldrup95@gmail.com>
2015-08-23 22:09:09 +0200

    anim stuff
    -added idle, walk and attack anim

--- Assets/Art/Animations/charAnimController.cs
+++ Assets/Art/Animations/charAnimController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class charAnimController : StateMachineBehaviour {
 
+
     // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
 	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 	//
